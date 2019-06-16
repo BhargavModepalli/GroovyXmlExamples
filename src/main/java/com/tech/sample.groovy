2daf8da081822dabdce9e4e37429808479e9b917
BhargavModepalli <bhargavryl@gmail.com>
2019-06-16 18:11:07 -0400

    added modifications for slurped and parsed xml cases

--- src/main/java/com/tech/sample.groovy
+++ src/main/java/com/tech/sample.groovy
@@ -3,10 +3,10 @@ package com.tech
 
 class sample {
 
-     static void main(String[] args){
+    static void main(String[] args){
 
 
-          final String books = '''
+        final String books = '''
     <t version-api="2.0">
         <locations>
           <cities>
@@ -32,63 +32,81 @@ class sample {
     </t>
 '''
 
-         def resSlurped = new XmlSlurper().parseText(books)
-         def resParsed = new XmlParser().parseText(books)
+        def resSlurped = new XmlSlurper().parseText(books)
+        def resParsed = new XmlParser().parseText(books)
 
-         assert  resSlurped.locations.cities.city[0].name == 'chicago'
-         assert  resParsed.locations.cities.city[0].name.text() == 'chicago'
+        assert  resSlurped.locations.cities.city[0].name == 'chicago'
+        assert  resParsed.locations.cities.city[0].name.text() == 'chicago'
 
-         //Slurped Xml--sorting based on number so sfo has least number ascending
+        //Slurped Xml--sorting based on number so sfo has least number ascending
 
-         def sortSlurpAscening =  resSlurped.locations.cities.city.findAll { it.@id.toInteger() > 0}.sort {it.number}
+        def sortSlurpAscening =  resSlurped.locations.cities.city.findAll { it.@id.toInteger() > 0}.sort {it.number}
 
-         println("**** printing slurped ascending order number****  ${ sortSlurpAscening} ")
-         assert sortSlurpAscening[0].name == 'sfo'
+        println("**** printing slurped ascending order number****  ${ sortSlurpAscening} ")
+        assert sortSlurpAscening[0].name == 'sfo'
 
-         //Slurped Xml--sorting based on number so sfo has least number descending
+        //Slurped Xml--sorting based on number so sfo has least number descending
 
-         def sortSlurpDescening =  resSlurped.locations.cities.city.findAll { it.@id.toInteger() > 0}.sort { a,b -> b.number.toInteger() <=>a.number.toInteger() }
+        def sortSlurpDescening =  resSlurped.locations.cities.city.findAll { it.@id.toInteger() > 0}.sort { a,b -> b.number.toInteger() <=>a.number.toInteger() }
 
-         println("**** printing slurped descending order number****  ${ sortSlurpDescening} ")
+        println("**** printing slurped descending order number****  ${ sortSlurpDescening} ")
 
-         assert sortSlurpDescening[0].name == 'chicago'
-
-
-         //Parsed Xml----sorting based on number so sfo has least number ascending
-
-         def sortParseAscening =  resSlurped.locations.cities.city.findAll { it.@id.toInteger() > 0}.sort {it.number}
-
-         println("### printing parsed ascending order number ###  ${ sortParseAscening} ")
+        assert sortSlurpDescening[0].name == 'chicago'
 
 
-         assert sortSlurpAscening[0].name == 'sfo'
+        //Parsed Xml----sorting based on number so sfo has least number ascending
 
-         // Parsed Xml--sorting based on number so sfo has least number descending
+        def sortParseAscening =  resSlurped.locations.cities.city.findAll { it.@id.toInteger() > 0}.sort {it.number}
 
-         def sortParseDescening =  resSlurped.locations.cities.city.findAll { it.@id.toInteger() > 0}.sort { a,b -> b.number.toInteger() <=>a.number.toInteger() }
-
-         println("### printing slurped ascending order number ####  ${ sortParseDescening} ")
-
-         assert sortSlurpDescening[0].name == 'chicago'
-
-         /*
-         * Manipulate the xml
-          */
-/*
-         def parser = new XmlParser()
-
-       //  resParsed.appendNode( new QName("bahrgav added"),[id:"10"], "modepalliadded")
-          // parser.createNode(resParsed.locations.cities.city, new QName("bahrgav added"),[id:"10"])
-         resParsed.locations.cities.children().find{it.name = 'cities'}.appendNode{city(id: "45"){
-             number("123")
-             name("assinfsncodnnc")
-         }}
+        println("### printing parsed ascending order number ###  ${ sortParseAscening} ")
 
 
-         def sortParseDesceningafter =  resParsed.locations.cities.city.findAll { it.@id.toInteger() > 2}
-         println("### ************printing slurped ascending order number ####  ${ sortParseDesceningafter} ")
+        assert sortSlurpAscening[0].name == 'sfo'
 
-*/
+        // Parsed Xml--sorting based on number so sfo has least number descending
+
+        def sortParseDescening =  resSlurped.locations.cities.city.findAll { it.@id.toInteger() > 0}.sort { a,b -> b.number.toInteger() <=>a.number.toInteger() }
+
+        println("### printing slurped ascending order number ####  ${ sortParseDescening} ")
+
+        assert sortSlurpDescening[0].name == 'chicago'
+
+        /*
+           * Modifying the xml
+        */
+
+        //parsing xml modifications--once we modify the content it will immediately update the parsed xml
+
+        resParsed.locations.cities.city[0].replaceNode{
+            city(id:"1"){
+                name("chicago modified")
+                number("1515")
+            }
+
+        }
+        assert resParsed.locations.cities.city[0].name.text() == 'chicago modified'
+        assert resParsed.locations.cities.city[0].number.text() == '1515'
+
+        //Slurping xml modifications - even though if you modify the xml the content will be not updated utill we slurp again
+        resSlurped.locations.cities.city[0].replaceNode{
+            city(id:"1"){
+                name("chicago modified")
+                number("1515")
+            }
+
+        }
+
+        assert resSlurped.locations.cities.city[0].name.text() == 'chicago'
+        assert resSlurped.locations.cities.city[0].number.text() == '15'
+
+        def resSlurpedTwicenew =new  XmlSlurper().parseText(new groovy.xml.StreamingMarkupBuilder().bind { mkp.yield resSlurped }.toString())
+
+        assert resSlurpedTwicenew.locations.cities.city[0].name.text() == 'chicago modified'
+        assert resSlurpedTwicenew.locations.cities.city[0].number.text() == '1515'
+
+
+        print("****done****")
+
 
 
 
